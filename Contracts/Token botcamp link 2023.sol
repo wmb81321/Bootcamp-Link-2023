fe2b82dc2021aff4c54a7270f2947bd94a702b95
William Martinez <68308482+wmb81321@users.noreply.github.com>
2023-04-05 19:45:48 -0500

    Add files via upload

---
+++ Contracts/Token botcamp link 2023.sol
@@ -0,0 +1,16 @@
+//INICIO
+
+
+//SPDX-License-Identifier: MIT
+pragma solidity 0.5.2;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol';
+
+contract Token is ERC20Mintable{
+        string public name = "BOOTCAMP WMB8";
+        string public symbol = "WMBT2023";
+        uint8 public decimals = 2; 
+}
+
+
+//FIN
